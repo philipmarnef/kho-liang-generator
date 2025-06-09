d636663957417450e5f430748deb4fb989fe959f
Philip Marnef <philip@bureaumarnef.be>
2025-06-09 11:09:17 +0200

    First working version

--- src/app.js
+++ src/app.js
@@ -3,6 +3,13 @@ import Alpine from "alpinejs";
 const W_TILE = 10
 const W_JOINT = .25
 
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+}
+
 export default class App {
 
 	constructor () {
@@ -21,6 +28,56 @@ export default class App {
 				templateRows: 'repeat(24, 1fr)',
 				aspectRatio: '1.064257028',
 
+				collection: {
+
+					tile16900: {
+						active: true,
+						distribution: 4,
+					},
+					tile16905: {
+						active: true,
+						distribution: 4,
+					},
+					tile16906: {
+						active: true,
+						distribution: 4,
+					},
+					tile16901ne: {
+						active: true,
+						distribution: 1,
+					},
+					tile16901se: {
+						active: true,
+						distribution: 1,
+					},
+					tile16901sw: {
+						active: true,
+						distribution: 1,
+					},
+					tile16901nw: {
+						active: true,
+						distribution: 1,
+					},
+
+				},
+
+				distribution: function () {
+					let total = 0;
+					for ( const property in this.collection ) {
+						const tile = this.collection[property]
+						if ( tile.active ) total += parseInt(tile.distribution)
+					}
+					return total;
+				},
+
+				distributionString: function (tile) {
+					if ( !tile.active ) return '';
+					const total = this.distribution()
+					const pct = Math.round(tile.distribution / total * 10000) / 100
+
+					return `${tile.distribution} / ${total} (${pct}%)`
+				},
+
 				calculateGrid: function () {
 					this.columns = Math.ceil( this.width / ( W_TILE + W_JOINT ) )
 					this.rows = Math.floor( this.height / ( W_TILE + W_JOINT ) )
@@ -30,12 +87,35 @@ export default class App {
 				generateTiles: function () {
 					this.$refs.tiles.replaceChildren()
 					const q = this.columns * this.rows
-					console.log(q)
+
 					for( let i = 0; i < q; i++ ) {
 						const d = document.createElement('div')
-						d.classList.add('tile')
 						this.$refs.tiles.append(d)
 					}
+				},
+				shuffleTiles: function () {
+					const a = Array.from(this.$refs.tiles.children)
+					shuffleArray(a)
+					a.forEach( $e => $refs.tiles.append($e) )
+				},
+				distribute: function () {
+					let classes = []
+					for ( const tilename in this.collection ) {
+						const tile = this.collection[tilename]
+						if ( tile.active ) {
+							const n = Math.ceil( tile.distribution / this.distribution() * this.$refs.tiles.childElementCount )
+							for ( let i = 0; i < n; i++ ) {
+								classes.push(tilename)
+							}
+						}
+					}
+					shuffleArray(classes)
+
+					Array.from(this.$refs.tiles.children).forEach( $tile => {
+						$tile.className = ''
+						$tile.classList.add( classes.pop() )
+					})
+
 				},
 
 				resize: function () {
@@ -46,6 +126,7 @@ export default class App {
 				update: function () {
 					this.calculateGrid()
 					this.generateTiles()
+					this.distribute()
 				}
 
 			}))
